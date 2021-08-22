b12847549c131d6486f5c172b0c730ef11099b65
Shikhar Sharma <ceoshikhar@gmail.com>
2021-08-22 14:32:09 +0530

    feat: add logout button for temporary use

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { isServer } from "@/utils";
+import { Button } from "@/ui/Button";
 
 function Home() {
   const router = useRouter();
@@ -11,13 +12,20 @@ function Home() {
     if (!isAuth) {
       router.push("/login");
     }
-  }, []);
+  }, [isAuth, router]);
 
   // TODO: We will render the <App />
   if (isAuth) {
     return (
       <>
         <h1>You are logged in as {username}</h1>
+        <Button
+          text="Logout"
+          onClick={() => {
+            window.localStorage.removeItem("access-token");
+            window.location.reload();
+          }}
+        />
       </>
     );
   }
