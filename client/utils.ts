d024d55d1ec033f0d08ca3d10c3426efb3409e89
Shikhar Sharma <ceoshikhar@gmail.com>
2021-08-29 01:17:20 +0530

    replace nextjs app with electron-cra app

--- client/utils.ts
+++
@@ -1,8 +0,0 @@
-export function px2em(valInPx: number): string {
-  const valInEm = valInPx / 16;
-  return valInEm + "em";
-}
-
-export function isServer() {
-  return typeof window === "undefined";
-}
