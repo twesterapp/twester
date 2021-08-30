c651f67264a6e30c86d4b63c330f55e9f0ae010f
Shikhar Sharma <ceoshikhar@gmail.com>
2021-08-31 02:02:45 +0530

    feat: add icons for sidebar

---
+++ client/src/renderer/ui/Icons.ts
@@ -0,0 +1,7 @@
+import { FaHome, FaEye, FaSignOutAlt } from 'react-icons/fa';
+
+export { IconType } from 'react-icons';
+
+export { FaHome as IconHome };
+export { FaEye as IconEye };
+export { FaSignOutAlt as IconSignOut };
