37c862870af3c2c1bd75b3e553dd61f615f7aaa1
Shikhar Sharma <ceoshikhar@gmail.com>
2021-08-31 02:02:20 +0530

    fix: add types for props.theme in styled compononents

---
+++ client/src/renderer/typings/styled-components.d.ts
@@ -0,0 +1,8 @@
+import { darkTheme } from '../ui';
+
+type ThemeInterface = typeof darkTheme;
+
+declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface DefaultTheme extends ThemeInterface {}
+}
