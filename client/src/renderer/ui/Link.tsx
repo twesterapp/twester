5da954b790b2943db184d076ca5d1b4d04c5a035
Shikhar Sharma <ceoshikhar@gmail.com>
2021-09-05 22:19:47 +0530

    feat: add Link with custom styles

---
+++ client/src/renderer/ui/Link.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link as RouterLink, LinkProps } from 'react-router-dom';
+import styled from 'styled-components';
+
+export function Link(props: LinkProps) {
+  return (
+    <StyledLink>
+      <RouterLink {...props} />
+    </StyledLink>
+  );
+}
+
+const StyledLink = styled.span`
+  a {
+    color: ${(props) => props.theme.color.primary};
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
--- client/src/renderer/ui/index.ts
+++ client/src/renderer/ui/index.ts
@@ -3,6 +3,7 @@ export * from './Button';
 export * from './InputText';
 export * from './Spinner';
 export * from './Icons';
+export * from './Link';
 export { default as GlobalStyle } from './theme/GlobalStyle';
 export * from './theme/theme';
 export * from './Tooltip';
