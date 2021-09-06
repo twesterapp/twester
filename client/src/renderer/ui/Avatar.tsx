ed21d90d7f3cac7f2db62eab8c77534ddd3a0355
Shikhar Sharma <ceoshikhar@gmail.com>
2021-09-06 17:26:57 +0530

    feat: avatar border can be disabled via border prop

--- client/src/renderer/ui/Avatar.tsx
+++ client/src/renderer/ui/Avatar.tsx
@@ -3,12 +3,25 @@ import styled from 'styled-components';
 import defaultAvatar from '../assets/default-avatar.png';
 
 interface AvatarOptions extends React.ImgHTMLAttributes<HTMLImageElement> {
+  border?: boolean;
   src: string;
   size?: number;
 }
 
-export function Avatar({ size = 32, src, ...rest }: AvatarOptions) {
-  return <StyledImage {...rest} src={src ?? defaultAvatar} size={size} />;
+export function Avatar({
+  size = 48,
+  border = true,
+  src,
+  ...rest
+}: AvatarOptions) {
+  return (
+    <StyledImage
+      {...rest}
+      border={border}
+      src={src ?? defaultAvatar}
+      size={size}
+    />
+  );
 }
 
 const StyledImage = styled.img<AvatarOptions>`
@@ -19,6 +32,7 @@ const StyledImage = styled.img<AvatarOptions>`
   box-sizing: border-box;
   border-radius: 50%;
   padding: 0.15rem;
-  border: 0.15rem solid ${(props) => props.theme.color.primary};
+  border: ${(props) =>
+    props.border && `0.15rem solid ${props.theme.color.primary}`};
   user-select: none;
 `;
