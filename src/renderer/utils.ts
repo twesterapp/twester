5ebd5ebdfefd8ac7caca8ed6c0553d74fb54add8
Shikhar Sharma <ceoshikhar@gmail.com>
2021-09-12 21:39:30 +0530

    fix: show days and hours only if they are greater than 1

--- src/renderer/utils.ts
+++ src/renderer/utils.ts
@@ -75,9 +75,17 @@ export function abortAllSleepingTasks() {
 
 // Converts minutes to `Ad Bh Cm` format where A = days, B = hours, C = mins
 export function formatMinutesToString(mins: number): string {
-    const formattedString = `${Math.floor(mins / 24 / 60)}d ${Math.floor(
-        (mins / 60) % 24
-    )}h ${mins % 60}m`;
+    const days = Math.floor(mins / 24 / 60);
+    const hours = Math.floor((mins / 60) % 24);
+    const _mins = Math.floor(mins % 60);
+
+    // 3d 21h 43m
+    // `0m` only for `m`. If it's `0h 42m`, we ignore `0h`. Same for `0d`
+    let formattedString = '';
+
+    if (days) formattedString += `${days}d`;
+    if (hours || (!hours && days)) formattedString += ` ${hours}h`;
+    formattedString += ` ${_mins}m`;
 
     return formattedString;
 }
