0f1722362a597643956a8d6436585696f8ebc649
Shikhar Sharma <ceoshikhar@gmail.com>
2021-09-13 16:00:34 +0530

    add IconStart and IconClock

--- src/renderer/ui/Icons.tsx
+++ src/renderer/ui/Icons.tsx
@@ -7,6 +7,8 @@ import {
     FaUsers,
     FaEye,
     FaPowerOff,
+    FaClock,
+    FaStar,
 } from 'react-icons/fa';
 import { ImCross } from 'react-icons/im';
 import { IconBaseProps } from 'react-icons';
@@ -28,3 +30,5 @@ export function IconPlus({ style, ...rest }: IconBaseProps) {
 export { FaPlay as IconPlay };
 export { FaPause as IconPause };
 export { FaPowerOff as IconPower };
+export { FaClock as IconClock };
+export { FaStar as IconStar };
