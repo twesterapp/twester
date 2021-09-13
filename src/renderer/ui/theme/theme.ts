149636691de81e524404d05780341328635b6d55
Shikhar Sharma <ceoshikhar@gmail.com>
2021-09-13 15:23:15 +0530

    new color palette for theme

--- src/renderer/ui/theme/theme.ts
+++ src/renderer/ui/theme/theme.ts
@@ -2,33 +2,43 @@ interface Color {
     background: string;
     background2: string;
     background3: string;
-    background4: string;
+    brightBlue: string;
+    borderOnDisabled: string;
     disabled: string;
     error: string;
+    onDisabled: string;
     onPrimary: string;
+    onPrimaryHover: string;
     primary: string;
+    secondary: string;
     success: string;
-    textPrimary: string;
+    textAlter: string;
     textFaded: string;
+    textPrimary: string;
 }
 
 export interface Theme {
-    name: 'dark' | 'light';
+    name: 'dark';
     color: Color;
 }
 
 export const darkTheme: Theme = {
     name: 'dark',
     color: {
-        background: '#18171F',
-        background2: '#242329',
-        background3: '#2A2931',
-        background4: '#34333A',
-        disabled: '#464649',
+        background: '#00171F',
+        background2: '#003459',
+        background3: '#003F6B',
+        brightBlue: '#8ECAE6',
+        borderOnDisabled: '#0066AE',
+        disabled: '#00477A',
         error: '#FF5252',
+        onDisabled: '#A0A0A0',
         onPrimary: '#FFFFFF',
+        onPrimaryHover: '#1484CF',
         primary: '#3498DB',
+        secondary: '#FFB703',
         success: '#0AD48B',
+        textAlter: '#040F0F',
         textPrimary: '#F1F1F1',
         textFaded: '#979797',
     },
