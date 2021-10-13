d59a327dee4f7ff4ec0fcd6d27399ae1ff1bad73
Shikhar Sharma <ceoshikhar@gmail.com>
2021-10-13 20:09:07 +0530

    tests: add tests for error situations in Store

--- src/__tests__/store.test.ts
+++ src/__tests__/store.test.ts
@@ -4,20 +4,21 @@ interface StoreState {
     count: number;
 }
 
-function getInitialState(): StoreState {
+function initializer(): StoreState {
     return { count: 0 };
 }
 
-const testStore = new Store('test', getInitialState);
+const testStore = new Store('test');
+testStore.initStore(initializer);
 
 describe('tests Store class', () => {
     it('creates a Store instance', () => {
-        const store = new Store('test', getInitialState);
+        const store = new Store('test');
         expect(store instanceof Store).toBeTruthy();
     });
 
     it('gets store name', () => {
-        expect(testStore.name).toEqual('test-store');
+        expect(testStore.storeName).toEqual('test-store');
     });
 
     it('gets state from store', () => {
@@ -28,7 +29,20 @@ describe('tests Store class', () => {
     it('updates state in store', () => {
         const newCount = testStore.store.getState().count + 1;
         testStore.store.setState({ count: newCount });
-
         expect(testStore.store.getState().count).toEqual(newCount);
+    });
+
+    it('should throw an error if Store.store() called before Store.init()', () => {
+        const testStore = new Store('test');
+        expect(() => testStore.store).toThrow(
+            `Store is not initialized: Store.store() was called before calling Store.init()`
+        );
+    });
+
+    it('should throw an error if Store.useStore() called before Store.init()', () => {
+        const testStore = new Store('test');
+        expect(() => testStore.useStore()).toThrow(
+            `Store is not initialized: Store.useStore() was called before calling Store.init()`
+        );
     });
 });
