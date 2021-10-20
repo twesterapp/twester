c171a3325a3d5313e5cf68b3281830db33fe4789
Shikhar Sharma <ceoshikhar@gmail.com>
2021-10-20 22:10:28 +0530

    chore: remove oauthClient bearerClient from api

--- src/renderer/api.ts
+++ src/renderer/api.ts
@@ -5,95 +5,40 @@ import { logging } from 'renderer/core/logging';
 const log = logging.getLogger('API');
 
 const token = localStorage.getItem('access-token') || '';
-const PORT = '42069';
+const NODE_PORT = '42069';
 
 export const nodeClient = axios.create({
-    baseURL: `http://localhost:${PORT}`,
+    baseURL: `http://localhost:${NODE_PORT}`,
 });
-
-export const oauthClient = axios.create({
-    headers: {
-        'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
-    },
-});
-
-export const bearerClient = axios.create({
-    headers: {
-        'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
-    },
-});
-
-oauthClient.interceptors.request.use(
-    (config) => {
-        if (token) {
-            config.headers.Authorization = `OAuth ${token}`;
-        }
-
-        return config;
-    },
-    (err) => {
-        return Promise.reject(err);
-    }
-);
-
-oauthClient.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        if (
-            error.response.status === 401 ||
-            error.response.data.error === 'Unauthorized'
-        ) {
-            auth.signout();
-        }
-
-        return error;
-    }
-);
-
-bearerClient.interceptors.request.use(
-    (config) => {
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
-    },
-    (err) => {
-        return Promise.reject(err);
-    }
-);
-
-bearerClient.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        if (
-            error.response.status === 401 ||
-            error.response.data.error === 'Unauthorized'
-        ) {
-            auth.signout();
-        }
-
-        return error;
-    }
-);
 
 export async function makeGraphqlRequest(
     data: Record<string, unknown>
 ): Promise<Record<string, any>> {
-    return oauthClient({
+    return axios({
         method: 'POST',
         url: 'https://gql.twitch.tv/gql',
+        headers: {
+            'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
+            Authorization: `OAuth ${token}`,
+        },
         data,
     })
         .then((res) => {
             const data = res.data;
             return data;
         })
-        .catch((e) => {
-            log.error('Error while making Twitch GraphQL request:\n', e);
+        .catch((error) => {
+            log.error(
+                `Failed to make Twitch GraphQL request [${error?.response?.status}]: ${error?.response?.data?.message}`
+            );
+
+            if (
+                error.response.status === 401 ||
+                error.response.data.error === 'Unauthorized'
+            ) {
+                // The token has probably expired and there is no way to refresh
+                // the token, that's why we signout the user.
+                auth.signout();
+            }
         });
 }
